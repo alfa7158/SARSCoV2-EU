a82f2f95e90238b2ff39d9c3e2b83c44c21d5f6a
Abdullah A A <abdullah_al_faraj@windowslive.com>
2022-01-02 01:22:45 +0300

    working on multiple parts of the project such as xml and adding new things

---
+++ app/src/main/java/com/example/covideu/view/Selection/mainSelect/vaccineAndTreatment/ClinicalMainSelectFragment.kt
@@ -0,0 +1,38 @@
+package com.example.covideu.view.Selection.mainSelect.vaccineAndTreatment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
+import com.example.covideu.R
+
+
+class ClinicalMainSelectFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_clinical_main_select, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+            val clinicalTreatmentButton:ImageView = view.findViewById(R.id.allCinicalSelectButton)
+
+        clinicalTreatmentButton.setOnClickListener {
+
+
+            findNavController().navigate(R.id.action_clinicalMainSelectFragment_to_clinicalTreatmentFragment)
+        }
+
+    }
+
+
+}
